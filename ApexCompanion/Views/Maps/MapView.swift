9f665bef64a4f010baa0a4de3f5c03bda0c2fa71
Mitch Sharber <mitchkylell@gmail.com>
2020-08-25 18:45:30 -0400

    Fixed much of the bugginess with map zoom in feature. The map can now be panned horizontally and vertically while zoomed in. The map can longer be zoomed out past the original scale and the map cannot be zoomed in more the 2.5 of the original scale.

--- ApexCompanion/Views/Maps/MapView.swift
+++ ApexCompanion/Views/Maps/MapView.swift
@@ -20,7 +20,7 @@ struct MapView: View {
     
     var body: some View {
         GeometryReader {reader in
-            Image(self.mapImageName).resizable().scaledToFit().animation(.default).offset(x: self.draggedSize.width, y: 0).scaleEffect(self.scale).scaleEffect(self.isTapped ? 2 : 1, anchor: UnitPoint(x: self.pointTapped.x / reader.frame(in: .global).maxX, y: self.pointTapped.y / reader.frame(in: .global).maxY)).gesture(TapGesture(count: 2).onEnded({
+            Image(self.mapImageName).resizable().scaledToFit().animation(.default).offset(x: self.draggedSize.width, y: self.draggedSize.height).scaleEffect(self.scale).scaleEffect(self.isTapped ? 2 : 1, anchor: UnitPoint(x: self.pointTapped.x / reader.frame(in: .global).maxX, y: self.pointTapped.y / reader.frame(in: .global).maxY)).gesture(TapGesture(count: 2).onEnded({
             self.isTapped = !self.isTapped
         }).simultaneously(with: DragGesture(minimumDistance: 0, coordinateSpace: .global).onChanged({ (value) in
             self.pointTapped = value.startLocation
@@ -28,10 +28,20 @@ struct MapView: View {
         }).onEnded({ (value) in
             let offsetWidth = (reader.frame(in: .global).maxX * self.scale - reader.frame(in: .global).maxX) / 2
             let newDraggedWidth = self.draggedSize.width * self.scale
-            if (newDraggedWidth > offsetWidth) {
+            let offsetHeight = (reader.frame(in: .global).maxY * self.scale - reader.frame(in: .global).maxY) / 8
+            let newDraggedHeight = self.draggedSize.height * self.scale
+            if (newDraggedWidth > offsetWidth && newDraggedHeight > offsetHeight) {
+                self.draggedSize = CGSize(width: offsetWidth / self.scale, height: offsetHeight / self.scale)
+            } else if (newDraggedWidth < -offsetWidth && newDraggedHeight < -offsetHeight) {
+                self.draggedSize = CGSize(width: -offsetWidth / self.scale, height: -offsetHeight / self.scale)
+            } else if (newDraggedWidth > offsetWidth) {
                 self.draggedSize = CGSize(width: offsetWidth / self.scale, height: value.translation.height + self.previousDragged.height)
             } else if (newDraggedWidth < -offsetWidth) {
                 self.draggedSize = CGSize(width: -offsetWidth / self.scale, height: value.translation.height + self.previousDragged.height)
+            } else if (newDraggedHeight > offsetHeight) {
+                self.draggedSize = CGSize(width: value.translation.width + self.previousDragged.width, height: offsetHeight / self.scale)
+            } else if (newDraggedHeight < -offsetHeight) {
+                self.draggedSize = CGSize(width: value.translation.width + self.previousDragged.width, height: -offsetHeight / self.scale)
             } else {
                 self.draggedSize = CGSize(width: value.translation.width + self.previousDragged.width, height: value.translation.height + self.previousDragged.height)
             }
@@ -39,7 +49,14 @@ struct MapView: View {
             }))).gesture(MagnificationGesture().onChanged({ (scale) in
             self.scale = scale.magnitude
         }).onEnded({ (scaleFinal) in
-            self.scale = scaleFinal.magnitude
+            if scaleFinal < 1 {
+                self.scale = 1
+            }
+            else if scaleFinal > 2.5 {
+                self.scale = 2.5
+            } else {
+                self.scale = scaleFinal.magnitude
+            }
         }))
         }
     }
